70c1eb7da79de8e612ca1ae260418959de0936bb
jaspreet singh <71896519+jaspreet7037@users.noreply.github.com>
2021-11-23 22:02:26 +0530

    add

--- poseEstimationMIn.py
+++ poseEstimationMIn.py
@@ -2,16 +2,36 @@ import cv2
 import mediapipe as mp
 import time
 
+mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
+"""def __init__(self,
+static_image_mode=False,
+model_complexity=1,
+smooth_landmarks=True,
+enable_segmentation=False,
+smooth_segmentation=True,
+min_detection_confidence=0.5,
+min_tracking_confidence=0.5)"""
 pose = mpPose.Pose()
 
+wCam, hCam = 1280, 720
+
 cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
 pTime = 0
 while True: 
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
     print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 4, (255, 0, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
@@ -21,3 +41,6 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+
+
